a484fdfcc63ca3feca5ba1aeaed5d8abc01f3c70
Julien Bergner <julien@bergner.fr>
2012-11-14 19:51:00 +0100

    Apply bootstrap on interviewer views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121114135230) do
+ActiveRecord::Schema.define(:version => 20121114172031) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -30,5 +30,33 @@ ActiveRecord::Schema.define(:version => 20121114135230) do
 
   add_index "admins", ["email"], :name => "index_admins_on_email", :unique => true
   add_index "admins", ["reset_password_token"], :name => "index_admins_on_reset_password_token", :unique => true
+
+  create_table "interviewers", :force => true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "street"
+    t.string   "street_number"
+    t.string   "zip"
+    t.string   "mobile_phone"
+    t.date     "date_of_birth"
+    t.boolean  "drivers_license"
+    t.string   "bank_account_number"
+    t.string   "bank_identification_code"
+    t.datetime "created_at",                               :null => false
+    t.datetime "updated_at",                               :null => false
+    t.string   "email",                    :default => "", :null => false
+    t.string   "encrypted_password",       :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",            :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+  end
+
+  add_index "interviewers", ["email"], :name => "index_interviewers_on_email", :unique => true
+  add_index "interviewers", ["reset_password_token"], :name => "index_interviewers_on_reset_password_token", :unique => true
 
 end
