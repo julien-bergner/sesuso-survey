639079ba8df0423602ad819a0781dfd7d9407e25
Julien Bergner <julien@bergner.fr>
2012-11-14 16:47:11 +0100

    Set root path

--- config/routes.rb
+++ config/routes.rb
@@ -52,7 +52,7 @@ Sesuso::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'front_end#index'
 
   # See how all your routes lay out with "rake routes"
 
