9947b03a035de84b3dd4eb8da0d2919ec4217489
Julien Bergner <julien@bergner.fr>
2012-11-28 16:02:47 +0100

    Add validation to survey_location model

--- app/models/survey_location.rb
+++ app/models/survey_location.rb
@@ -2,5 +2,6 @@ class SurveyLocation < ActiveRecord::Base
   attr_accessible :city, :description, :map_url, :notice, :street, :street_number, :survey_area_id, :title, :url, :zip
 
   belongs_to :survey_area
+  validates_presence_of :title
 
 end
