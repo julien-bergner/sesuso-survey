ed8b1227f700cc327caf24d626fb0458b331bbf2
Julien Bergner <julien@bergner.fr>
2012-11-28 16:35:23 +0100

    Add validation to survey model

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,6 +1,10 @@
 class Survey < ActiveRecord::Base
   attr_accessible :city, :description, :end_date, :start_date, :title
 
+  validates_presence_of :title
+  validates_presence_of :start_date
+  validates_presence_of :end_date
+
   has_many :survey_areas
   has_many :survey_locations, :through => :survey_areas
 end
