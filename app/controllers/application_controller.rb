c03627850d4c5331c7003ce5ad49fcf8caaba9a9
Julien Bergner <julien@bergner.fr>
2012-11-29 23:52:17 +0100

    Adapt abilities for agent role

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) ||
+        if resource.is_a?(User) && resource.has_role?(:admin)
+          surveys_url
+        else if resource.is_a?(User) && resource.has_role?(:agent)
+          survey_areas_url
+          end
+        end
+  end
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
