813a7f03494825dfb0acaf55a28633242a73f345
Julien Bergner <julien@bergner.fr>
2012-12-02 05:51:34 +0100

    Pretty up, wire up and translate survey_value_set views

--- app/models/survey_value_set.rb
+++ app/models/survey_value_set.rb
@@ -2,4 +2,7 @@ class SurveyValueSet < ActiveRecord::Base
   attr_accessible :survey_data_set_id, :value1, :value2, :value3, :value4, :value5, :value6, :value7
 
   belongs_to :survey_data_set
+  has_one :survey_location, :through => :survey_data_set
+  has_one :survey_area, :through => :survey_location
+  has_one :survey, :through => :survey_area
 end
